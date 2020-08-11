ad166db23cb660d24f2729523eddb880a4e1766d
SatyaDileep <SatyaDileepKumar.Thotakura@in.pega.com>
2020-08-11 21:18:11 +0530

    Updates

---
+++ precache-manifest.495b9c9aeca0d183700c6e9a0a7cd4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cdcd4efd6851093b0b2798e9b203f2",
+    "url": "/my-task-list/index.html"
+  },
+  {
+    "revision": "5a50a6e3d7480b4e765a",
+    "url": "/my-task-list/static/css/main.10e012b2.chunk.css"
+  },
+  {
+    "revision": "66d5476dbdfb580d3105",
+    "url": "/my-task-list/static/js/2.aa508749.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/my-task-list/static/js/2.aa508749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a50a6e3d7480b4e765a",
+    "url": "/my-task-list/static/js/main.cb3f1bbb.chunk.js"
+  },
+  {
+    "revision": "4bc1baeaa93f7a7496d3",
+    "url": "/my-task-list/static/js/runtime-main.add9a482.js"
+  }
+]);
